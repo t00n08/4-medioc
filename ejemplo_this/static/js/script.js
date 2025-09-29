c23b4409fa9c8ede06e12eb697761d523e50135b
t00n08 <castromartin@liceovvh.cl>
2025-09-29 10:54:26 -0300

    ok

--- ejemplo_this/static/js/script.js
+++ ejemplo_this/static/js/script.js
@@ -1,17 +1,29 @@
-function ejemplo(elemento) {
-    alert("like recibido", elemento);
-}
 
-function turnOff(element) {
-    if(element.innerText == "Login"){
+function turnOff(element){
+    if (element.innerText == "Login"){
         element.innerText = "Logout";
-    }else{
+    } else{
         element.innerText = "Login";
     }
-    
 }
 
-function hide(element) {
+function Hide(element){
     element.remove();
 }
 
+
+
+
+function Like(element){
+    const span = element.querySelector("span");
+    let like1 =parseInt(span.innerText);
+    like1++;
+    span.innerText = like1 + " Like(s)";   
+}
+
+function Like2(element){
+    const span = element.querySelector("span");
+    let like2 =parseInt(span.innerText);
+    like2++;
+    span.innerText = like2 + " Like(s)";   
+}
